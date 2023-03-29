78e9a5e33a904d6c696d349aba28386cb8f90f96
CandelaReynoso <mariacandelamusica@gmail.com>
2023-03-29 13:19:04 -0300

    ruta PUT ok

--- api/src/controllers/dogEdit.js
+++ api/src/controllers/dogEdit.js
@@ -9,14 +9,45 @@ const dogEdit = async (id,name, age, height, weight, image, temperament) => {
     // Actualizar los datos del perro en la base de datos
     await dogToUpdate.update({
                 name:name,  
-                age: age, 
+                age:age, 
                 height:height, 
                 weight:weight, 
                 image:image,
-                temperament: temperament
+                temperament:temperament
        })
     // Devolver los datos actualizados del perro.
     return dogToUpdate;
 };
 
-module.exports = { dogEdit };
+module.exports = { dogEdit }; 
+
+
+
+
+
+
+
+
+
+/* const { Dog } = require('../db');
+
+const dogEdit = async (id,name, age, height, weight, image, temperament) => {
+    // Verificar que el perro a actualizar existe en la base de datos
+    const dogToUpdate = await Dog.findByPk(id);
+    if(!dogToUpdate) {
+        throw new Error ('There is no info to update');
+    }
+    // Actualizar los datos del perro en la base de datos
+    await dogToUpdate.update({
+                name=name,  
+                age= age, 
+                height=height, 
+                weight=weight, 
+                image=image,
+                temperament= temperament
+       })
+    // Devolver los datos actualizados del perro.
+    return dogToUpdate;
+};
+
+module.exports = { dogEdit }; */
