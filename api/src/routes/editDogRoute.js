1e883c79ec807e546540560ec33e5cdb32a0ca2e
CandelaReynoso <mariacandelamusica@gmail.com>
2023-03-29 13:21:45 -0300

    PUT funciona

--- api/src/routes/editDogRoute.js
+++ api/src/routes/editDogRoute.js
@@ -16,21 +16,3 @@ router.put('/update/:id', async (req, res) => {
 
 module.exports = router;
 
-
-/* const { Router } = require('express');
-const router = Router();
-const { dogEdit } = require('../controllers/dogEdit');
-
-router.put('/update/:id', async (req, res) => {
-    const { id }  = req.params; // Obtener el ID del perro a actualizar.
-    const { name, age, height, weight, image, temperament } = req.body; // Obtener los nuevos datos del perro.
-    try {
-        const updateDog = await dogEdit(id, name, age, height, weight, image, temperament);
-        res.status(200).json(updateDog);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error in updating dog' });
-    }
-});
-
-module.exports = router; */
